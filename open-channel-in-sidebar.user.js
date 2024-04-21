6c6a23c51de52630f3e6cc6636ccb897d6362307
Maxamax1 <s-kleeberg@web.de>
2024-04-21 17:49:21 +0200

    Update open-channel-in-sidebar.user.js

--- open-channel-in-sidebar.user.js
+++ open-channel-in-sidebar.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Open Channel from Sidebar
 // @namespace    http://tampermonkey.net
-// @version      1.0.2
+// @version      1.0.3
 // @description  Opens the channel page, when clicking on the creator in the sidebar
 // @author       Maxamax
 // @match        https://www.youtube.com/*
@@ -13,7 +13,7 @@
 
 (function () {
     
-    if (!(window.location.href.indexOf("youtube") > -1)) {
+    if (!(window.location.hostname === "youtube.com")) {
         return;
     }
 
